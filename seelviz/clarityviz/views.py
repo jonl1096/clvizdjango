d7a6e3b856be0e2508d09361df17c56e0088b3ff
Jonathan Liu <jonathan.jy.liu@gmail.com>
2017-01-22 20:37:50 -0500

    fixed small bug

--- seelviz/clarityviz/views.py
+++ seelviz/clarityviz/views.py
@@ -435,7 +435,8 @@ def imgGet(inToken, ori1):
         print "inverse affine"
 
         # Saving annotations
-        imgWrite(inAnnoImg, 'img/' + inToken + '/' + inToken + '_anno.nii')
+        anno_path = 'img/' + inToken + '/' + inToken + '_anno.nii'
+        imgWrite(inAnnoImg, str(anno_path))
         # ndImg = sitk.GetArrayFromImage(inAnnoImg)
         # sitk.WriteImage(inAnnoImg, location)
     print "generated output"
@@ -446,16 +447,7 @@ def imgGet(inToken, ori1):
 def image_parse(inToken, ori1, num_points):
     start = time.time()
     # imgGet is where the token name changes to adding the 'reorient_atlas'
-    imgName, tupleResolution = imgGet(inToken,ori1)
-    # imgName = imgGet(inToken)
-    run_time = time.time() - start
-    print('imgGet time = %f' % run_time)
-
-    
-    # imgName = inToken + 'reorient_atlas'
-    copydir = os.path.join(os.getcwd(), os.path.dirname('img/'))
-    print('copydir: %s' % copydir)
-    print('imgName: %s' % imgName)
+    imgName, tupleResolution = imgGet(inToken,ori1) # imgName = imgGet(inToken) run_time = time.time() - start print('imgGet time = %f' % run_time) # imgName = inToken + 'reorient_atlas' copydir = os.path.join(os.getcwd(), os.path.dirname('img/')) print('copydir: %s' % copydir) print('imgName: %s' % imgName)
     img = claritybase(imgName, copydir)       # initial call for clarityviz
     print "loaded into claritybase"
     img.loadEqImg()
