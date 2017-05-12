185e7c08e07f7db7853257d6932f5f3489e0586b
Jonathan Liu <jonathan.jy.liu@gmail.com>
2017-05-12 14:01:53 -0400

    removed orientation

--- seelviz/clarityviz/migrations/0002_auto_20161114_1514.py
+++ seelviz/clarityviz/migrations/0002_auto_20161114_1514.py
@@ -25,7 +25,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('token', models.CharField(max_length=20)),
-                ('orientation', models.CharField(max_length=3)),
+                ('bucket', models.CharField(max_length=3)),
                 ('num_points', models.CharField(max_length=20)),
             ],
         ),
--- seelviz/clarityviz/views.py
+++ seelviz/clarityviz/views.py
@@ -52,7 +52,7 @@ class OutputView(generic.DetailView):
 
 class ComputeCreate(CreateView):
     model = Compute
-    fields = ['token', 'orientation', 'num_points']
+    fields = ['token', 'bucket', 'num_points']
 
     def form_valid(self, form):
 
